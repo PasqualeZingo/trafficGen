0562b62a318704f8c727c217be52cfcf9bc01765
Luke <lucasd@udel.edu>
2020-07-23 13:50:38 +0000

    updated email.py to allow addition of a subject line

--- email.py
+++ email.py
@@ -1,11 +1,9 @@
 import os
-def sendEmail(sender,target,body):
+def sendEmail(sender,target,subject,body):
 	f = open("email.txt","w")
-	cmd = "MAIL FROM:" + sender + "\nRCPT TO:" + target + "\ndata\n" + body + "\n.\n"
+	cmd = "MAIL FROM:" + sender + "\nRCPT TO:" + target + "\ndata\n" + "subject: " + subject + "\n\n" + body + "\n.\n"
 	f.write(cmd)
 	f.close()
 	os.system("telnet studio.lrd.com 25 < email.txt")
 	os.system("rm email.txt")
 	return "sent"
-
-print(sendEmail("God","sales@example.com","init"))
