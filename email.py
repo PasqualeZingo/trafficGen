7fa3aefc1f369704b78d9bbd497a19d9a9b7c3da
Lucasd <lucasd@udel.edu>
2020-07-24 15:00:43 +0000

    updated email.py to use SASL authentication

--- email.py
+++ email.py
@@ -1,9 +1,10 @@
 import os
-def sendEmail(sender,target,subject,body):
+from base64 import b64encode
+def sendEmail(sender,pwd,target,subject,body):
 	f = open("email.txt","w")
-	cmd = "MAIL FROM:" + sender + "\nRCPT TO:" + target + "\ndata\n" + "subject: " + subject + "\n\n" + body + "\n.\n"
+	cmd = "EHLO there\nAUTH PLAIN " + b64encode(b'\x00' + sender.encode() + b'\x00' + pwd.encode()).decode() + '\n' + "MAIL FROM:" + sender + "\nRCPT TO:" + target + "\ndata\n" + "subject: " + subject + "\n\n" + body + "\n.\n"
 	f.write(cmd)
 	f.close()
-	os.system("telnet studio.lrd.com 25 < email.txt")
+	os.system("telnet studio.lrd.com 587 < email.txt")
 	os.system("rm email.txt")
 	return "sent"
