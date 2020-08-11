d5475a295ac601301e4995abf2d837e3dcfa27d4
= <=>
2020-08-11 10:56:33 -0400

    Added getIP() funciton to sendEmail agent.

--- agents/emailSender.py
+++ agents/emailSender.py
@@ -1,6 +1,6 @@
 import os
 from base64 import b64encode
-def sendEmail(sender,pwd,target,subject,body):
+def sendEmail(sender,target,subject,body,pwd="password"):
 	f = open("email.txt","w")
 	cmd = "EHLO there\nAUTH PLAIN " + b64encode(b'\x00' + sender.encode() + b'\x00' + pwd.encode()).decode() + '\n' + "MAIL FROM:" + sender + "\nRCPT TO:" + target + "\ndata\n" + "subject: " + subject + "\n\n" + body + "\n.\n"
 	f.write(cmd)
@@ -8,3 +8,12 @@ def sendEmail(sender,pwd,target,subject,body):
 	os.system("telnet studio.lrd.com 587 < email.txt")
 	os.system("rm email.txt")
 	return "sent"
+
+def getIP():
+    os.system("ifconfig | grep inet | awk -F' ' '{print $2}' | head -n 1 > .ip")
+    f = open(".ip","r")
+    ip = f.read().strip()
+    f.close()
+    return ip
+
+sendEmail("s" + getIP() + "@example.com","info@example.com","test","init")
