3aead6314ca409f137678a2d785dd53163bfb604
lucasd <lucasd@udel.edu>
2020-08-11 14:35:08 +0000

    tweaked printFile.py

--- agents/printFile.py
+++ agents/printFile.py
@@ -1,6 +1,6 @@
 import os
 def printFile(fname: str):
 	f = fname + ".pdf"
-	os.system("lp -U 'reciever' -h 'printer.luked.com' -d 'VPDF' " + f)
+	os.system("lp -U 'reciever' -h 'cups_pdf.luked.com' -d 'VPDF' " + f)
 	return None
-
+printFile("printThis")
