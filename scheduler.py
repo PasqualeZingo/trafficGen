0d5401d14abfa391e8f1c9d04285a449289cd615
= <=>
2020-08-27 13:25:19 -0400

    Documented imports for scheduler.py. Removed redundant 'import os' command.

--- scheduler.py
+++ scheduler.py
@@ -1,14 +1,16 @@
-import time
-import numpy as np
-import os
-from runall import startNAS, runall
+import time #For sleeping between script executions
+import numpy as np #For generating random numbers between 0 and 1.
+from runall import startNAS, runall #For starting scripts on the traffic_gen_box containers and adding a pool and a dataset to the FreeNAS server. See runall.__doc__ for more details.
 
+#Add a pool and a dataset to the freenas storage server on the network.
 startNAS()
+#Wait for the script activated by the previous command to finish.
 time.sleep(10)
 alive = True
 run = True # Get from docker run signal
 scale = 1
 while alive:
+    #Generate a random number between 0 and 1.
     nxt = np.random.uniform()
     if not run:
         time.sleep(10)
@@ -17,9 +19,11 @@ while alive:
         print("Call random duckduckgo query")
         #Executes userAgent.py on all traffic_gen_boxes.
         runall(".query")
+        #Wait an extra 5 seconds, since this script is particularly slow.
         time.sleep(5)        
     elif nxt < .5:
         print("Interact with storage")
+        #Executes freeNASagent.py on all traffic_gen_boxes.
         runall(".freenasagent.sh")
     elif nxt < .75:
         print("Call send Email")
@@ -31,8 +35,9 @@ while alive:
         runall(".print.sh")
     elif nxt >= 0.95:
         print("Done for now")
-        #Terminates the program.
+        #Terminates the while loop.
         alive = False
+    #Wait for a random number of seconds.
     wait = np.random.exponential(scale)
     time.sleep(wait)
     #wait an extra 10 seconds to make sure the executed script is complete before attempting to run another.
