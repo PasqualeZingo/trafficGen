4cf4c07af8c17a418d13e6b842de9b4b3fc2079c
Lukerd-29-00 <53152087+Lukerd-29-00@users.noreply.github.com>
2020-11-11 13:36:29 -0500

    Update addDisk.sh

--- netBuilder/addDisk/addDisk.sh
+++ netBuilder/addDisk/addDisk.sh
@@ -6,7 +6,7 @@ then
     python3 getID.py node $1 $2 > tmp.txt
     nid=$(cat tmp.txt)
     rm -f tmp.txt
-    #change the /data/ to whatever the path to your gns3 directory is. Change nothing after that.
+    #change the /data/ to whatever the path to your gns3 directory is. Change "/usa/lucasd" to some folder you have read/write permission in.
     cp "/data/gns3/projects/$pid/project-files/qemu/$nid/hda_disk.qcow2" /usa/lucasd
     cd /usa/lucasd
     mv hda_disk.qcow2 "$3.qcow2"
